5268e3c27eaa6a332c14f83d7d3149c60ed303ca
Andrew Osborn <osbornandrewj@gmail.com>
2021-09-23 12:35:10 -0700

    commit tree 5c6e1fc4baf57a2aef9263d5a272dc1dd9458fdb

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+function superbowlWin(record){
+  // for (const results in record)
+   // if (record.result === "W") 
+    const winner = record.find(season => season.result === 'W')
+     if (winner)
+      {return winner.year}
+    else
+      return undefined
 
+
+}
